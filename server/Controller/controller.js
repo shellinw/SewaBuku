2b02a6d1ffc359527496a664556ac6b9331d748f
shellinw <143944980+shellinw@users.noreply.github.com>
2024-12-25 21:55:30 +0700

    feat:Get/books endpoint done

--- server/Controller/controller.js
+++ server/Controller/controller.js
@@ -1,1 +1,17 @@
 const { Buku } = require("../models/index");
+
+class Controller {
+    static async getBuku(req, res, next) {
+        try {
+            const dataBuku = await Buku.findAll();
+            res.status(200).json({
+                message: "Success",
+                data: dataBuku,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+module.exports = Controller;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const Controller = require("./Controller/controller");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Welcome to the Express server!");
-});
+app.get("/books", Controller.getBuku);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
