73dcbf5d1ab2ea35d7d89868557eacc63def6b77
shellinw <143944980+shellinw@users.noreply.github.com>
2024-12-29 20:24:29 +0700

    feat:replaced alert with ui

--- client/src/Component/BukuCard.jsx
+++ client/src/Component/BukuCard.jsx
@@ -6,18 +6,28 @@ const BukuCard = ({ buku }) => {
     const [tglKembali, setTglKembali] = useState("");
     const [totalSewa, setTotalSewa] = useState(null);
     const [isFormVisible, setIsFormVisible] = useState(false);
-
+    const [successAlert, setSuccessAlert] = useState(false);
+    const [errMessage, setErrMessage] = useState("");
     const handleSewaClick = () => {
         setIsFormVisible(true);
     };
 
+    const SuccessAlert = () => {
+        return <div className="success-alert">Sewa Berhasil!</div>;
+    };
+
+    const ErrorAlert = () => {
+        return <div className="error-alert">{errMessage}</div>;
+    };
+
     const confirmSewa = () => {
         if (!totalSewa) {
-            alert("Error: Tanggal masih kosong.");
+            setErrMessage("Error: Tanggal masih kosong.");
         } else {
-            alert("Sukses Sewa Buku");
+            setSuccessAlert(true);
 
             setTimeout(() => {
+                setSuccessAlert(false);
                 window.location.reload();
             }, 1000);
         }
@@ -26,7 +36,9 @@ const BukuCard = ({ buku }) => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         if (!tglPinjam || !tglKembali) {
-            alert("Error:Tanggal Pinjam dan Tanggal Kembali Belum diisi.");
+            setErrMessage(
+                "Error:Tanggal Pinjam dan Tanggal Kembali Belum diisi."
+            );
             return;
         }
 
@@ -49,75 +61,82 @@ const BukuCard = ({ buku }) => {
             const data = await response.json();
             setTotalSewa(data.tarif);
         } catch (error) {
-            alert(`Error: ${error.message}`);
+            setErrMessage(error.message);
         }
     };
 
     return (
-        <div className="book-card" key={id}>
-            <div className="book-title">
-                Judul:
-                <div className="content-title">{judul}</div>
-            </div>
-            <div className="book-author">Penulis: {penulis}</div>
-            <div className="book-price">
-                Tarif / Hari:
-                <div>{formatRupiah(tarifPerHari)}</div>
-            </div>
-            {isFormVisible === true ? (
-                <></>
-            ) : (
-                <>
-                    {" "}
-                    <button className="rent-button" onClick={handleSewaClick}>
-                        Sewa
-                    </button>
-                </>
-            )}
-
-            {isFormVisible && (
-                <form onSubmit={handleSubmit} className="rent-form">
-                    <div className="label-date">
-                        <label htmlFor="tglPinjam">Start Date</label>
-                        <input
-                            type="date"
-                            id="tglPinjam"
-                            value={tglPinjam}
-                            onChange={(e) => setTglPinjam(e.target.value)}
-                        />
-                    </div>
-                    <div className="label-date">
-                        <label htmlFor="tglKembali">Return Date</label>
-                        <input
-                            type="date"
-                            id="tglKembali"
-                            value={tglKembali}
-                            onChange={(e) => setTglKembali(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <button className="confirm-button" type="submit">
-                            Calculate Total Rent
-                        </button>
-                    </div>
-                </form>
-            )}
-
-            {totalSewa !== null && (
-                <div className="total-rent">
-                    Total Sewa: <strong>{formatRupiah(totalSewa)}</strong>
+        <>
+            <div className="book-card" key={id}>
+                {successAlert && <SuccessAlert />}
+                {errMessage && <ErrorAlert />}
+                <div className="book-title">
+                    Judul:
+                    <div className="content-title">{judul}</div>
                 </div>
-            )}
-            {isFormVisible === false ? (
-                <></>
-            ) : (
-                <>
-                    <button className="rent-button" onClick={confirmSewa}>
-                        Sewa Sekarang
-                    </button>
-                </>
-            )}
-        </div>
+                <div className="book-author">Penulis: {penulis}</div>
+                <div className="book-price">
+                    Tarif / Hari:
+                    <div>{formatRupiah(tarifPerHari)}</div>
+                </div>
+                {isFormVisible === true ? (
+                    <></>
+                ) : (
+                    <>
+                        {" "}
+                        <button
+                            className="rent-button"
+                            onClick={handleSewaClick}
+                        >
+                            Sewa
+                        </button>
+                    </>
+                )}
+
+                {isFormVisible && (
+                    <form onSubmit={handleSubmit} className="rent-form">
+                        <div className="label-date">
+                            <label htmlFor="tglPinjam">Start Date</label>
+                            <input
+                                type="date"
+                                id="tglPinjam"
+                                value={tglPinjam}
+                                onChange={(e) => setTglPinjam(e.target.value)}
+                            />
+                        </div>
+                        <div className="label-date">
+                            <label htmlFor="tglKembali">Return Date</label>
+                            <input
+                                type="date"
+                                id="tglKembali"
+                                value={tglKembali}
+                                onChange={(e) => setTglKembali(e.target.value)}
+                            />
+                        </div>
+                        <div>
+                            <button className="confirm-button" type="submit">
+                                Calculate Total Rent
+                            </button>
+                        </div>
+                    </form>
+                )}
+
+                {totalSewa !== null && (
+                    <div className="total-rent">
+                        Total Sewa: <strong>{formatRupiah(totalSewa)}</strong>
+                    </div>
+                )}
+                {isFormVisible === false ? (
+                    <></>
+                ) : (
+                    <>
+                        <button className="rent-button" onClick={confirmSewa}>
+                            Sewa Sekarang
+                        </button>
+                    </>
+                )}
+            </div>
+        </>
     );
 };
 export default BukuCard;
